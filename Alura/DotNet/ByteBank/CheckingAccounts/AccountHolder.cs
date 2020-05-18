6eb3d530251d26eb6219263d7f9d290d364978e8
lucas.torra <lucasgabrielft@hotmail.com>
2020-05-18 17:44:57 -0300

    Implementation CheckingAccount

---
+++ Alura/DotNet/ByteBank/CheckingAccounts/AccountHolder.cs
@@ -0,0 +1,22 @@
+﻿namespace ByteBank
+{
+    public class AccountHolder
+    {
+        private string _personID;
+
+        public string Name { get; set; }
+        public string PersonID
+        {
+            get
+            {
+                return _personID;
+            }
+            set
+            {
+                // Escrevo minha lógica de validação de PersonID
+                _personID = value;
+            }
+        }
+        public string Occupation { get; set; }
+    }
+}
---
+++ Alura/DotNet/ByteBank/CheckingAccounts/CheckingAccount.cs
@@ -0,0 +1,90 @@
+﻿// using _05_ByteBank;
+
+namespace ByteBank
+{
+    public class CheckingAccount
+    {
+        public AccountHolder AccountHolder { get; set; }
+
+        public static int TotalAccounts { get; private set; }
+
+
+        private int _branchNumber;
+        public int BranchNumber
+        {
+            get
+            {
+                return _branchNumber;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    return;
+                }
+
+                
+                _branchNumber = value;
+            }
+        }
+        public int AccountNumber { get; set; }
+
+        private double _balance = 100;
+
+        public double Balance
+        {
+            get
+            {
+                return _balance;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _balance = value;
+            }
+        }
+
+
+        public CheckingAccount(int branchNumber, int accountNumber)
+        {
+            BranchNumber = branchNumber;
+            AccountNumber = accountNumber;
+
+            TotalAccounts++;
+        }
+
+
+        public bool WithDraw(double amount)
+        {
+            if (_balance < amount)
+            {
+                return false;
+            }
+
+            _balance -= amount;
+            return true;
+        }
+
+        public void Deposit(double amount)
+        {
+            _balance += amount;
+        }
+
+
+        public bool Transfer(double amount, CheckingAccount targetAccount)
+        {
+            if (_balance < amount)
+            {
+                return false;
+            }
+
+            _balance -= amount;
+            targetAccount.Deposit(amount);
+            return true;
+        }
+    }
+}
